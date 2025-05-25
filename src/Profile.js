636b35d234f1a991abe66762fed89dfb9f86ce05
USER <cristhian20023@gmail.com>
2025-05-25 14:53:26 -0500

    agregamos un array, actualizamos y agregamos boton, actualizar pantalla, agregamos el componente para contar los likes

--- src/Profile.js
+++ src/Profile.js
@@ -1,62 +1,65 @@
-import { useState } from 'react';
+// src/Profile.js
+import React, { useState } from 'react';
 
 const users = [
     {
         id: 1,
         name: 'Hedy Lamarr',
         imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-        imageSize: 90,
+        imageSize: 90
     },
     {
         id: 2,
         name: 'Alan Turing',
         imageUrl: 'https://i.imgur.com/MK3eW3As.jpg',
-        imageSize: 90,
+        imageSize: 90
     },
     {
         id: 3,
         name: 'Ada Lovelace',
         imageUrl: 'https://i.imgur.com/npXxJ25s.jpg',
-        imageSize: 90,
-    },
+        imageSize: 90
+    }
 ];
 
-function ProfileCard({ user }) {
-    const [likes, setLikes] = useState(0);
-
-    function handleLike() {
-        setLikes(likes + 1);
-    }
-
-    return (
-        <div key={user.id} style={{ marginBottom: '30px' }}>
-            <h3>{user.name}</h3>
-            <img
-                className="avatar"
-                src={user.imageUrl}
-                alt={'Foto de ' + user.name}
-                style={{
-                    width: user.imageSize,
-                    height: user.imageSize,
-                    borderRadius: '50%',
-                }}
-            />
-            <div style={{ marginTop: '10px' }}>
-                <button onClick={handleLike}>❤️ Like</button>
-                <span style={{ marginLeft: '10px' }}>Likes: {likes}</span>
-            </div>
-        </div>
-    );
-}
-
 export default function Profile() {
+    const [likes, setLikes] = useState(
+        users.reduce((acc, user) => {
+            acc[user.id] = 0;
+            return acc;
+        }, {})
+    );
+
+    const handleLike = (userId) => {
+        setLikes((prevLikes) => ({
+            ...prevLikes,
+            [userId]: prevLikes[userId] + 1
+        }));
+    };
+
     return (
         <div>
             <h2>Perfiles</h2>
-            {users.map(user => (
-                <ProfileCard key={user.id} user={user} />
+            {users.map((user) => (
+                <div key={user.id} style={{ marginBottom: '30px' }}>
+                    <h3>{user.name}</h3>
+                    <img
+                        src={user.imageUrl}
+                        alt={'Foto de ' + user.name}
+                        style={{
+                            width: user.imageSize,
+                            height: user.imageSize,
+                            borderRadius: '50%'
+                        }}
+                    />
+                    <div style={{ marginTop: '10px' }}>
+                        <button onClick={() => handleLike(user.id)}>❤️ Me gusta</button>
+                        <span style={{ marginLeft: '10px' }}>
+              Me gusta: {likes[user.id]}
+            </span>
+                    </div>
+                </div>
             ))}
         </div>
     );
 }
-
